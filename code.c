abe555c8dafc0739b4877e05d3dee4ac16f19a74
sudhakar <sudhakar123@gmail.com>
2017-08-03 08:41:31 +0530

    added code file

---
+++ code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a,b,c;
+	printf(enter value for a = ");
+	scanf("%d",&a)
+	printf("enter value for b =");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("the addition of two numbers is = %d",c)
+getche();
+}
